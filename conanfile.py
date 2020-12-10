d560fb5401864fcdee83852044ed5579aa47e4e1
Karl Wallner <kwallner@mail.de>
2020-12-10 19:18:52 +0100

    Update to version 6.1.2. Use new innoextract instead of innounpack.

--- conanfile.py
+++ conanfile.py
@@ -6,7 +6,7 @@ from subprocess import call
 
 class InnoSetupConan(ConanFile):
     name = "InnoSetup"
-    version = "6.0.5"
+    version = "6.1.2"
     license = "..."
     license_url = "..."
     url = "..."
@@ -18,20 +18,19 @@ class InnoSetupConan(ConanFile):
     no_copy_source = True
     _iss_pack= "%s-%s.exe" % (name.lower(), version)
     _iss_url = "https://mlaan2.home.xs4all.nl/ispack/%s" % (_iss_pack)
-    
-    def build_requirements(self):
-        self.build_requires("InnoSetupUnpacker/0.49@%s/%s" % (self.user, self.channel))
-
+    _iss_unpack= "%s-%s.zip" % (name.lower(), version)
+   
     def source(self):
+        tools.download("https://constexpr.org/innoextract/files/innoextract-1.9-windows.zip", self._iss_unpack)
         tools.download(self._iss_url, self._iss_pack)
+        tools.unzip(self._iss_unpack)
 
     def build(self):
-        call(["innounp.exe", "-x", os.path.join(self.source_folder, self._iss_pack)])
-        os.remove("install_script.iss")
-        shutil.rmtree("{tmp}")
+        call([os.path.join(self.source_folder, "innoextract.exe"), "-e", os.path.join(self.source_folder, self._iss_pack)])
+        shutil.rmtree("tmp")
         
     def package(self):
-        self.copy(pattern="*", src= "%s/{app}" % self.build_folder)
+        self.copy(pattern="*", src= "%s/app" % self.build_folder)
                     
     def package_info(self):
         self.env_info.PATH.insert(0, self.package_folder)
